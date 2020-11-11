b3aa32ee1a0b272800b2585d90783d46710ce16e
frogchou <frogchou@163.com>
2020-11-11 15:57:39 +0800

    Update rsyncinstall.sh

--- rsyncinstall.sh
+++ rsyncinstall.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #install rsync
 set -e
+current_path=$(cd "$(dirname $0)";pwd)
 if [ -z "`dpkg -l |grep rsync`" ];then
 	sudo apt install rsync
 fi
 echo "install rsync over"
 mkdir /etc/rsyncd
-#sudo wget -P /etc/rsyncd "https://github.com/frogchou/data_syn/tree/master/rsyncd/rsyncd.conf"
-#sudo wget -P /etc/rsyncd "https://github.com/frogchou/data_syn/tree/master/rsyncd/rsyncd.motd"
-#sudo wget -P /etc/rsyncd "https://github.com/frogchou/data_syn/tree/master/rsyncd/rsyncd.secrets"
-chmod 600 /etc/rsyncd.passwd
+#sudo wget -P /etc/rsyncd "https://raw.githubusercontent.com/frogchou/data_syn/master/rsyncd/rsyncd.conf"
+#sudo wget -P /etc/rsyncd "https://raw.githubusercontent.com/frogchou/data_syn/master/rsyncd/rsyncd.motd"
+#sudo wget -P /etc/rsyncd "https://raw.githubusercontent.com/frogchou/data_syn/master/rsyncd/rsyncd.secrets"
+
+cp ${current_path}/rsyncd/* /etc/rsyncd/
+
+chmod 600 /etc/rsyncd/rsyncd.passwd
 echo "install over"
 echo "change /etc/rsyncd/rsyncd.conf to your self"
-echo "run commad 'rsync -daemon --config=/etc/rsyncd.conf' to start service"
+echo "run commad 'rsync -daemon --config=/etc/rsyncd/rsyncd.conf' to start service"
